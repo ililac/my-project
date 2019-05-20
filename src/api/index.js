38bbefbc8e8b6b118d24199416c1ddb8484eaae2
tianpeirong <canghaihongxin@163.com>
2019-05-20 17:25:26 +0800

    111

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ import { getRequest, postRequest, putRequest, deleteRequest, importRequest, uplo
 // Vaptcha ID
 export const vaptchaID = "5bb9a277fc650e00a4155567"
 // 文件上传接口
-export const uploadFile = "/xboot/upload/file"
+export const uploadFile = "/system/upload/file"
 // 验证码渲染图片接口
 export const drawCodeImage = "/zhfw/system/common/captcha/draw/"
 // 获取菜单
@@ -472,11 +472,11 @@ export const fromUp = (params) => {
     return postRequest('/system/u/info', params)
 }
 export const formSeve = (params) => {
-    return postRequest('/contractModel/save', params)
+    return postRequest('/contract/contractModel/save', params)
 }
 
 export const myDelete = (params) => {
-    return postRequest('/contractModel/del', params)
+    return postRequest('/contract/contractModel/del', params)
 }
 
 export const findContractByParam = (params) => {
