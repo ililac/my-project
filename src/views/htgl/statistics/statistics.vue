69297b21ca70275dd23c1ce1d455c3f8cbb86d07
lvzhuqing <lvzhuqing@chinalawinfo.com>
2019-05-23 10:41:45 +0800

    2019年5月23日10:41:38

--- src/views/htgl/statistics/statistics.vue
+++ src/views/htgl/statistics/statistics.vue
@@ -1,10 +1,10 @@
 <template>
-    <div>
+	<div>
 		<Card>
 			<Row type="flex" justify="space-between" class="code-row-bg">
 				<Col :span="24">
 					<Form ref="searchForm" :model="searchForm" inline :label-width="120" class="search-form">
-						<Form-item label="年份">
+						<Form-item label="合同生效日期">
 							<DatePicker
 									v-model="searchForm.year"
 									type="year"
@@ -14,86 +14,94 @@
 									placeholder="选择年份"
 									:editable="false"
 							></DatePicker>
-						</Form-item>
-						<Form-item label="月份">
-							<Select v-model="searchForm.month" style="width:200px">
+							<Select v-model="searchForm.month" style="width:200px;margin-left: 20px;">
 								<Option v-for="item in monthList" :value="item">{{ item }}</Option>
 							</Select>
+						</Form-item>
+						<Form-item>
+
 						</Form-item>
 						<Button @click="dateChange" type="primary" icon="ios-search">搜索</Button>
 					</Form>
 				</Col>
-        	</Row>
-        </Card>
-        <div>
-			<Row :gutter="24" :style="{marginTop: '10px'}">
+			</Row>
+		</Card>
+		<div>
+			<Row :gutter="12" :style="{marginTop: '10px'}">
 				<Col :span="6" :style="{marginBottom: '10px'}">
 					<Card class="info_card">
-					  <div slot="title">
+						<div slot="title">
 							审批合同总量
 							<Tooltip class="info_icon" placement="top">
 								<Icon type="ios-information-circle-outline" color="#108DE9" size="20"/>
-							  <div slot="content">当前时间范围内,经本院审批的合同总量</div>
+								<div slot="content">当前时间范围内,经本院审批的合同总量</div>
 							</Tooltip>
 						</div>
-					  <div class="info_con">
-					  	{{total.approveTotalSize}}份
-					  </div>
+						<div class="info_con">
+							{{total.approveTotalSize}}份
+						</div>
 					</Card>
 				</Col>
 				<Col :span="6" :style="{marginBottom: '10px'}">
 					<Card class="info_card">
-					  <div slot="title">
+						<div slot="title">
 							签署合同总量
 							<Tooltip class="info_icon" placement="top">
 								<Icon type="ios-information-circle-outline" color="#108DE9" size="20"/>
-							  <div slot="content">当前时间范围内,完成签署的合同总量</div>
+								<div slot="content">当前时间范围内,完成签署的合同总量</div>
 							</Tooltip>
 						</div>
-					  <div class="info_con">
-					  	{{total.approveTotalSize}}份
-					  </div>
+						<div class="info_con">
+							{{total.approveTotalSize}}份
+						</div>
 					</Card>
 				</Col>
 				<Col :span="6" :style="{marginBottom: '10px'}">
 					<Card class="info_card">
-					  <div slot="title">
+						<div slot="title">
 							收款总额
 							<Tooltip class="info_icon" placement="top">
 								<Icon type="ios-information-circle-outline" color="#108DE9" size="20"/>
-							  <div slot="content">当前时间范围内,已签署收款合同的总金额</div>
+								<div slot="content">当前时间范围内,已签署收款合同的总金额</div>
 							</Tooltip>
 						</div>
-					  <div class="info_con">
-					  	¥{{total.recieveResult.factmoney}}
-					  </div>
+						<div class="info_con">
+							¥{{total.recieveResult.factmoney}}
+						</div>
 						<div>已收金额¥{{total.recieveResult.actualmoney}}</div>
 						<Progress :percent="total.recieveResult.percent" hide-info/>
 					</Card>
 				</Col>
 				<Col :span="6" :style="{marginBottom: '10px'}">
 					<Card class="info_card">
-					  <div slot="title">
+						<div slot="title">
 							付款总额
 							<Tooltip class="info_icon" placement="top">
 								<Icon type="ios-information-circle-outline" color="#108DE9" size="20"/>
-							  <div slot="content">当前时间范围内,已签署付款合同的总金额</div>
+								<div slot="content">当前时间范围内,已签署付款合同的总金额</div>
 							</Tooltip>
 						</div>
-					  <div class="info_con">
-					  	¥{{total.payResult.factmoney}}
-					  </div>
+						<div class="info_con">
+							¥{{total.payResult.factmoney}}
+						</div>
 						<div>已付金额¥{{total.payResult.actualmoney}}</div>
 						<Progress :percent="total.payResult.percent" hide-info/>
 					</Card>
 				</Col>
 			</Row>
-            <Row :gutter="8">
-                <Col :span="12" :style="{marginBottom: '10px'}">
+			<Row :gutter="8">
+				<Col :span="12" :style="{marginBottom: '10px'}">
 					<Card>
 						<p slot="title" class="card-title">
 							<Icon type="md-map"></Icon>
 							新增审批合同
+							<Tooltip class="info_icon" placement="top">
+								<Icon type="ios-information-circle-outline" color="#108DE9" size="20"/>
+								<div slot="content">
+									<h3>每个时间单位内，经本院审批</h3>
+									<h3>的合同数量。</h3>
+								</div>
+							</Tooltip>
 						</p>
 						<div class="data-source-row">
 							<div class="visit">
@@ -108,6 +116,13 @@
 						<p slot="title" class="card-title">
 							<Icon type="md-map"></Icon>
 							新增签署合同
+							<Tooltip class="info_icon" placement="top">
+								<Icon type="ios-information-circle-outline" color="#108DE9" size="20"/>
+								<div slot="content">
+									<h3>每个时间单位内，完成签署的合</h3>
+									<h3>同数量。</h3>
+								</div>
+							</Tooltip>
 						</p>
 						<div class="data-source-row">
 							<div class="visit">
@@ -117,11 +132,18 @@
 						</div>
 					</Card>
 				</Col>
-					<Col :span="12" :style="{marginBottom: '10px'}">
+				<Col :span="12" :style="{marginBottom: '10px'}">
 					<Card>
 						<p slot="title" class="card-title">
 							<Icon type="md-map"></Icon>
 							合同类型分布
+							<Tooltip class="info_icon" placement="top">
+								<Icon type="ios-information-circle-outline" color="#108DE9" size="20"/>
+								<div slot="content">
+									<h3>当前时间范围内，经本院审批</h3>
+									<h3>的合同在不同合同类型的分布。</h3>
+								</div>
+							</Tooltip>
 						</p>
 						<div class="data-source-row">
 							<div class="visit">
@@ -131,11 +153,18 @@
 						</div>
 					</Card>
 				</Col>
-					<Col :span="12" :style="{marginBottom: '10px'}">
+				<Col :span="12" :style="{marginBottom: '10px'}">
 					<Card>
 						<p slot="title" class="card-title">
 							<Icon type="md-map"></Icon>
 							相对方分布（前五）
+							<Tooltip class="info_icon" placement="top">
+								<Icon type="ios-information-circle-outline" color="#108DE9" size="20"/>
+								<div slot="content">
+									<h3>当前时间范围内，经本院审批</h3>
+									<h3>的合同在不同相对方的分布。</h3>
+								</div>
+							</Tooltip>
 						</p>
 						<div class="data-source-row">
 							<div class="visit">
@@ -145,22 +174,22 @@
 						</div>
 					</Card>
 				</Col>
-            </Row>
-        </div>
-    </div>
+			</Row>
+		</div>
+	</div>
 </template>
 <script>
-    import echarts from 'echarts';
+	import echarts from 'echarts';
 	import {
 		statisticsDepartment
 	} from "@/api/index";
-    export default {
-        name: 'statistics',
-        components: {
-            // visiteVolume
-        },
-        data() {
-            return {
+	export default {
+		name: 'statistics',
+		components: {
+			// visiteVolume
+		},
+		data() {
+			return {
 				monthList:[],
 				relative_type:[],
 				total:{
@@ -169,15 +198,15 @@
 					payResult: {factmoney: 0, actualmoney: 0,percent:0},
 					recieveResult: {factmoney: 0, actualmoney: 0,percent:0}
 				},
-                searchForm: {
-                    year:"",
+				searchForm: {
+					year:"",
 					month:""
-                },
+				},
 				options: {
-                    disabledDate (date) {
-                        return date && date.valueOf() > Date.now() - 86400000;
-                    }
-                },
+					disabledDate (date) {
+						return date && date.valueOf() > Date.now() - 86400000;
+					}
+				},
 				optionShow:true,
 				optionShow2:true,
 				optionShow3:true,
@@ -186,15 +215,15 @@
 				visiteVolume2:"",
 				visiteVolume3:"",
 				visiteVolume4:""
-            }
-        },
-        methods: {
+			}
+		},
+		methods: {
 			//年份改变
 			yearChange(v){
 				console.log(v);
 				let date = new Date();
 				let year = date.getFullYear();
-				// this.searchForm.year = 
+				// this.searchForm.year =
 				this.monthList = [];
 				if(v == year){
 					for(let i = 0;i <= date.getMonth();i++){
@@ -212,10 +241,10 @@
 						data:[]
 					},
 					tooltip: {
-					  trigger: "axis",
-					  axisPointer: {
-					    type: "shadow"
-					  }
+						trigger: "axis",
+						axisPointer: {
+							type: "shadow"
+						}
 					},
 					grid: {
 						left: '3%',
@@ -237,7 +266,7 @@
 					series : []
 				};
 				const option2 = {
-				    tooltip: {
+					tooltip: {
 						trigger: 'axis'
 					},
 					xAxis:  {
@@ -257,7 +286,7 @@
 					]
 				};
 				const option3 = {
-				    tooltip : {
+					tooltip : {
 						trigger: 'item',
 						formatter: "{a} <br/>{b} : {c} ({d}%)"
 					},
@@ -290,11 +319,11 @@
 					]
 				};
 				const option4 = {
-				    tooltip : {
+					tooltip : {
 						trigger: 'item',
 						formatter: "{a} <br/>{b} : {c} ({d}%)"
 					},
-					
+
 					legend: {
 						bottom: 0,
 						left: 'center',
@@ -323,12 +352,12 @@
 							approveTotalSize:res.result.approveTotalSize,
 							signTotalSize:res.result.signTotalSize,
 							payResult:{
-								factmoney: res.result.payResult.factmoney, 
+								factmoney: res.result.payResult.factmoney,
 								actualmoney: res.result.payResult.actualmoney,
 								percent:(100*res.result.payResult.factmoney/res.result.payResult.actualmoney),
 							},
 							recieveResult:{
-								factmoney: res.result.recieveResult.factmoney, 
+								factmoney: res.result.recieveResult.factmoney,
 								actualmoney: res.result.recieveResult.actualmoney,
 								percent:(100*res.result.recieveResult.factmoney/res.result.recieveResult.actualmoney),
 							}
@@ -337,8 +366,8 @@
 						let arr = res.result.newApprovedContract.yValue;
 						for(var i = 0;i<arr.length;i++){
 							option.legend.data.push(arr[i].name);
-							arr[i].type = 'bar'; 
-							arr[i].stack = '广告'; 
+							arr[i].type = 'bar';
+							arr[i].stack = '广告';
 							option.series.push(arr[i]);
 						}
 						option2.xAxis.data = res.result.newSignedContract.xValue;
@@ -392,14 +421,14 @@
 							this.optionShow4 = false;
 						}
 						window.addEventListener('resize', function () {
-						    visiteVolume.resize();
-						    visiteVolume2.resize();
-						    visiteVolume3.resize();
-						    visiteVolume4.resize();
+							visiteVolume.resize();
+							visiteVolume2.resize();
+							visiteVolume3.resize();
+							visiteVolume4.resize();
 						});
 					}
 				})
-				
+
 			},
 			dateChange(){
 				let date = "";
@@ -418,16 +447,16 @@
 				this.init(date);
 			}
 		},
-        mounted() {
+		mounted() {
 			let date = new Date();
 			let d = date.getFullYear();
 			for(let i = 0;i <= date.getMonth();i++){
 				this.monthList.push(i+1);
 			};
 			this.searchForm.year = d+'';
-            this.init(d);
-        }
-    }
+			this.init(d);
+		}
+	}
 </script>
 <style lang="less" scoped>
 	.info_card{
@@ -440,7 +469,7 @@
 			.info_word{
 				font-size: 14px;
 			}
-			
+
 		}
 		.info_icon{
 			float: right;
@@ -450,6 +479,11 @@
 			font-size: 26px;
 			font-weight: 900;
 			text-align: center;
+		}
+	}
+	.card-title{
+		.info_icon{
+			float: right;
 		}
 	}
 	.visit{
